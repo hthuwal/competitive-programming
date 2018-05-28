641d59f8351d09b01b655e07f438d53799f4bb93
Harish Thuwal <hthuwal@gmail.com>
2018-05-28 14:17:00 +0530

    Added comments

--- plus_plus_may_circuit_18.cpp
+++ plus_plus_may_circuit_18.cpp
@@ -1,24 +1,45 @@
 #include <bits/stdc++.h>
 
+/**
+ * Data structure describing a plus sign
+ */
 typedef struct plus
 {
-	int cr, cc;
+	int cr, cc;  // row number and column number of the center of the plus
 	int v[5];
 } plus;
 
+
+/**
+ * List of pluses
+ */
 plus pluses[3000];
 
+
+/**
+ * Seems LIke an overkill check if doesnt work
+ */
 bool is_not_overlapped(plus a, plus b)
 {
+
+	// if center of both pluses are on the same row then there column should be at least three 3 units apart
 	if(a.cr == b.cr)
 		return abs(a.cc - b.cc) >= 3;
 
+	// else if center of both pluses are on the same col then there rows should be at least three 3 units apart
 	if(a.cc == b.cc)
 		return abs(a.cr - b.cr) >= 3;
 
+	// else either there cneter columns or their center rows should be 2 units apart
 	return abs(a.cr - b.cr) >= 2 || abs(a.cc - b.cc) >=2;
 }
 
+/**
+ * Find element wise product of two pluses
+ * @param  a plus1
+ * @param  b plus2
+ * @return plus1 * plus2 if they don't overlap otherwise returns 0
+ */
 int value(plus a, plus b)
 {
 	if (is_not_overlapped(a, b))
@@ -28,16 +49,22 @@ int value(plus a, plus b)
 
 int main()
 {
-	freopen("in.txt","r",stdin);
+	freopen("in.txt","r",stdin); // For reading input from in.txt directly, Comment this while submitting!
 	int r,c;
 	scanf("%d %d",&r,&c);
 	int a[r][c];
 
+	/**
+	 * Reading The matrix
+	 */
 	for(int i=0;i<r;i++)
 		for(int j=0;j<c;j++)
 			scanf("%d",&a[i][j]);
 
 	int k=0;
+	/**
+	 * Creating a list of all pluses
+	 */
 	for(int i=1;i<r-1;i++)
 		for(int j=1;j<c-1;j++)
 		{
@@ -52,22 +79,22 @@ int main()
 		}			
 
 	int ans = 0;
-	// plus hc, ad;
+
+	/**
+	 * Finding product of every possible pair of pluses, and taking the maximum out of it
+	 */
 	for(int i=0; i<k; i++)
 	{
 		for(int j=i+1;j<k;j++)
 		{
 			int temp = value(pluses[i], pluses[j]);
-			// printf("%d", temp);
 			if(temp > ans)
 			{
 				ans = temp;
-				// hc = pluses[i];
-				// ad = pluses[j];
 			}
 
 		}
 	}
-	// printf("%d (%d, %d) (%d, %d)", ans, hc.cr, hc.cc, ad.cr, ad.cc);
+
 	printf("%d", ans);
 }
