bfc41d52288086791dd8ea2ded7bad8e6797a914
Harish Thuwal <hthuwal@gmail.com>
2018-06-05 21:18:21 +0530

    Revert "Elevator Trouble SPOJ"
    Wrong file comitted
    This reverts commit 2dd4ef3ca59cd692b37617d6955c062fcab3c7fd.

--- test.py
+++ test.py
@@ -1,3 +1,35 @@
-a, b = list(map(int, input().split()))
-# c = a + b
-print(c)
+def memo(f):
+    memo = {}
+    
+    def inner(cls, i, final):
+        key = str(i)+str(final)
+        if key not in memo:
+            memo[key] = f(cls, i, final)
+        return memo[key]
+    return inner
+
+
+@memo
+def ans(cls, i, final):
+    if(i == len(cls)):
+        return 1
+    else:
+        hc = 0
+        if cls[i][0] >= final:
+            hc += ans(cls, i+1, cls[i][1])
+        hc += ans(cls, i+1, final)
+        return hc
+
+
+while(True):
+    cls = []
+    n = int(raw_input())
+    if n == -1:
+        break
+    for i in range(n):
+        a, b = raw_input().strip().split()
+        a = int(a)
+        b = int(b)
+        cls.append((a, b))
+    cls.sort(key=lambda x: x[1])
+    print "%08d" % ((ans(cls, 0, 0) - 1) % 100000000)
