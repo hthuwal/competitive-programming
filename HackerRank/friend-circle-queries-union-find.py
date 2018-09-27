0ded99ca2715c3be880884374ffc5444ee7f58b1
Harish Thuwal <hthuwal@gmail.com>
2018-09-27 15:37:22 +0530

    Size weighted union does work,
    finding max of sizes everytime was the culprit

--- HackerRank/friend-circle-queries-union-find.py
+++ HackerRank/friend-circle-queries-union-find.py
@@ -1,42 +1,45 @@
 
 parents = {}
 size = {}
+max_size = 1
 
 
-def find(a):
+def find_basic(a):
+    global max_size
     x = a
     while(a in parents and parents[a] != a):
         a = parents[a]
     if a not in parents:
         parents[a] = a
         size[a] = 1
+        if size[a] > max_size:
+            max_size = size[a]
 
-    # while(x in parents and parents[x] != x):
-    #     temp = x
-    #     x = parents[x]
-    #     parents[temp] = a
-    #     size[x] -= size[temp]
     return a
 
 
-def union(a, b):
-    pa = find(a)
-    pb = find(b)
+def size_weighted_union(a, b):
+    global max_size
+    pa = find_basic(a)
+    pb = find_basic(b)
+
     if (pa != pb):
         if(size[pa] > size[pb]):
             parents[pb] = pa
             size[pa] += size[pb]
             size[pb] = 0
+            if(size[pa] > max_size):
+                max_size = size[pa]
         else:
             parents[pa] = pb
             size[pb] += size[pa]
             size[pa] = 0
+            if(size[pb] > max_size):
+                max_size = size[pb]
+
 
 q = int(input())
 while(q > 0):
     a, b = input().strip().split()
-    union(a, b)
-    # print(parents)
-    # print(size)
-    print(max(list(size.values())))
+    size_weighted_union_find_path_compression(a, b)
     q -= 1
