a0b1f779ab83e49401b29f726bcfb1e00cbee60e
Harish Thuwal <hthuwal@gmail.com>
2019-03-24 15:37:09 +0530

    Add Another method: No improvement though

--- HackerEarth/huge-stones.py
+++ HackerEarth/huge-stones.py
@@ -47,11 +47,34 @@ def find_radius_v3(points, center):
     return sum(distances) / len(distances)
 
 
+def find_radius_v4(points, center):
+    """ Also gives 89.95 :( """
+    ws = [point[2] for point in points]
+    ds = [dist(point[:2], center) for point in points]
+    large = max(ds)
+    small = min(ds)
+
+    sum_wi_x_di = sum([di * wi for di, wi in zip(ds, ws)])
+    sum_wi_x_di_square = sum([di * di * wi for di, wi in zip(ds, ws)])
+    sum_wi = sum(ws)
+
+    ans = None
+
+    for i in range(1000):
+        cand_r = 0 + i * (large - small) / 1000
+        print(cand_r)
+        cand_w = sum_wi_x_di_square + (cand_r * cand_r) * sum_wi - 2 * cand_r * sum_wi_x_di
+        if ans is None or ans[1] > cand_w:
+            ans = (cand_r, cand_w)
+
+    return ans[0]
+
+
 if __name__ == '__main__':
     n = int(input().strip())
     points = [0] * n
     for i in range(n):
         points[i] = list(map(int, input().strip().split()))
     center = weighted_centroid(points)
-    radius = find_radius_v2(points, center)
+    radius = find_radius_v4(points, center)
     print(*center, radius)
