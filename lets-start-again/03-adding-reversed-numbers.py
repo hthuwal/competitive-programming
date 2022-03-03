737260244aa3773b4e7d378c74f87db1f253112c
Harish Thuwal <hthuwal@gmail.com>
2022-03-03 13:34:25 +0530

    Move old stuff to old folde

--- lets-start-again/03-adding-reversed-numbers.py
+++
@@ -1,8 +0,0 @@
-t = int(input().strip())
-while(t > 0):
-    a, b = input().strip().split()
-    a = int(''.join(reversed(a)))
-    b = int(''.join(reversed(b)))
-    ans = str(a + b).rstrip('0')
-    print(''.join(reversed(ans)))
-    t -= 1
--- lets-start-again/16-julka.py
+++
@@ -1,9 +0,0 @@
-# https://www.spoj.com/problems/JULKA/
-
-for i in range(10):
-    n = int(input().rstrip())
-    b = int(input().rstrip())
-    x = (n - b) // 2
-    print(x + b)
-    print(x)
-    n -= 1
