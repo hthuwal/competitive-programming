239529e92459b54acd86e3f0ce351daa258d2245
Harish Thuwal <hthuwal@gmail.com>
2022-08-26 00:57:16 +0530

    binary search

---
+++ leetcode/binary-search.cpp
@@ -0,0 +1,28 @@
+/* -------------------------------------------------------------------------- */
+/*                 https://leetcode.com/problems/binary-search                */
+/* -------------------------------------------------------------------------- */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
