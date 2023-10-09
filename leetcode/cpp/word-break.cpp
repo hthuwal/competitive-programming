3549e9245962993fe7d0185e8e4273569a69016a
Harish Thuwal <hthuwal@gmail.com>
2023-10-09 12:44:03 +0530

    Word Brek DP

--- leetcode/cpp/word-break.cpp
+++ leetcode/cpp/word-break.cpp
@@ -6,7 +6,7 @@
 #include <unordered_map>
 #include <vector>
 
-using namespaces std;
+using namespace std;
 
 // Recursive solution with memoisation
 class Solution {
@@ -49,7 +49,33 @@ class Solution {
         return possible;
     }
 
+    bool wordBreak(string s, vector<string>& wordDict) { return wordBreakRecursive(s, wordDict, 0); }
+};
+
+// Dynamic Programming Solution
+class Solution {
+   public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        return wordBreakRecursive(s, wordDict, 0);
+        int n = s.size();
+
+        bool dp[n];
+        memset(dp, false, sizeof(dp));
+
+        for (int i = n - 1; i >= 0; i--) {
+            const string& str = s.substr(i);
+            for (int j = 0; j < wordDict.size(); j++) {
+                const string& word = wordDict[j];
+                if (str.size() >= word.size() && word == str.substr(0, word.size())) {
+                    int left_from = i + word.size();
+
+                    // Nothing is left
+                    if (left_from >= n || dp[left_from]) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[0];
     }
 };
