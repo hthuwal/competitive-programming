63a65058cb499348dc79e579729538a80676e986
Harish Thuwal <hthuwal@gmail.com>
2024-01-25 10:49:24 +0530

    Kth largest two heap approaches

--- leetcode/cpp/kth-largest-element-in-an-array.cpp
+++ leetcode/cpp/kth-largest-element-in-an-array.cpp
@@ -7,31 +7,34 @@
 
 using namespace std;
 
-/* -------------------------------- O (nlogk) ------------------------------- */
-
-int findKthSmallest(vector<int>& nums, int k) {
-    std::priority_queue<int> max_heap;
-    for (int i = 0; i < nums.size(); i++) {
-        if (i < k) {
-            max_heap.push(nums[i]);
-        } else {
-            int top = max_heap.top();
-            if (nums[i] < top) {
-                max_heap.pop();
-                max_heap.push(nums[i]);
-            }
-        }
+// Build a Max Heap of all nums: O(n)
+// Pop k times to get the kth largest: O(k*log(n))
+// Overall Time Complexity: O(n + k * log(n))
+int solveSTLMaxHeap(vector<int>& nums, int k) {
+    std::priority_queue<int, vector<int>, std::less<int>> heap(nums.begin(), nums.end());
+    for (; k > 1; k--) {
+        heap.pop();
     }
-    return max_heap.top();
+    return heap.top();
 }
 
-
-int findKthLargestHeap(vector<int>& nums, int k) { return findKthSmallest(nums, nums.size() - k + 1); }
-
+// Build a Min heap of first k nums: O(k)
+// Iterate over next remaining nums, if they are less than top, remove top and add them: (n-k) * (log k)
+// Oerall Time compelxity: O(k + (n-k) log(k))
+int solveSTLMinHeap(vector<int>& nums, int k) {
+    std::priority_queue<int, vector<int>, std::greater<int>> heap(nums.begin(), nums.begin() + k);
+    for (int i = k; i < nums.size(); i++) {
+        if (nums[i] > heap.top()) {
+            heap.pop();
+            heap.push(nums[i]);
+        }
+    }
+    return heap.top();
+}
 
 int main() {
     vector<int> nums = {3, 2, 1, 5, 6, 4};
     int k = 2;
-    cout << findKthLargestHeap(nums, k) << endl;
+    cout << solveSTLMinHeap(nums, k) << endl;
     return 0;
 }
